c84e726b077fe792c997412d813030992b3b49ef
Julian Alvarez <juliancamiloalvarez77@gmail.com>
2023-05-26 10:21:26 -0500

    [events-manager]Enable cors

--- events-manager/infrastructure/http/server/server.go
+++ events-manager/infrastructure/http/server/server.go
@@ -1,6 +1,8 @@
 package http
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,5 +13,17 @@ import (
 func NewServer() *gin.Engine {
 	engine := gin.New()
 	engine.Use(gin.Recovery())
+	engine.Use(enableCors)
 	return engine
+}
+
+func enableCors(ctx *gin.Context) {
+	ctx.Writer.Header().Add("Access-Control-Allow-Methods", "*")
+	ctx.Writer.Header().Add("Access-Control-Allow-Origin", "*")
+	ctx.Writer.Header().Add("Access-Control-Allow-Headers", "*")
+	ctx.Writer.Header().Add("Access-Control-Max-Age", "3600")
+
+	if ctx.Request.Method == http.MethodOptions {
+		ctx.String(http.StatusOK, "")
+	}
 }
