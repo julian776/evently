a85d10b0e5617906d682f2775c6109128a137072
Julian Alvarez <juliancamiloalvarez77@gmail.com>
2023-05-26 07:30:18 -0500

    [Front]Add events component

---
+++ front/src/app/events/mappers/event.mapper.ts
@@ -0,0 +1,14 @@
+import { Event } from "../models/event";
+
+export const eventMapper = (event: Event): Event => {
+  const eventFormatted: Event = {
+    ...event
+  }
+  if (event.description.length >= 170) {
+    eventFormatted.description = event.description.split(' ', 26).join(' ') + '...'
+  }
+  if (event.title.length >= 70) {
+    eventFormatted.title = event.title.split(' ', 7).join(' ') + '...'
+  }
+  return eventFormatted
+}
---
+++ front/src/app/events/models/event.ts
@@ -0,0 +1,17 @@
+export type Event = {
+  id: string;
+
+  title: string;
+
+  description: string;
+
+  organizerName: string;
+
+  organizerEmail: string;
+
+  location: string;
+
+  startTime: string;
+
+  endTime: string;
+};
